d4f64d755c4ffeaa41ec22307b1f04518db57736
Kristof <janusk370@gmail.com>
2020-12-15 10:12:14 +0100

    Initial commit

---
+++ src/main/java/NowaKlasa.java
@@ -0,0 +1,4 @@
+public class NowaKlasa {
+    public void metoda() {
+    }
+}
---
+++ src/main/java/Test1.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Test1 {
+    private static Object String;
+
+
+    public static void main(String[] ags) {
+        NowaKlasa NowaKlasa = new NowaKlasa();
+        NowaKlasa.metoda();
+
+
+
+        System.out.println("Program testowy Kristof");
+
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        System.out.print("Hello jak masz na imię? ");
+        name = scanner.next();
+        System.out.print("Hello " + name );
+
+
+        String city;
+        System.out.print(" Skąd jesteś? ");
+        city = scanner.next();
+        System.out.print("To piękne miasto,");
+
+
+        String age;
+        System.out.print(" Ile masz lat ");
+        age = scanner.next();
+        System.out.print("Winaszuje!!!");
+        System.out.print("" + name);
+        System.out.print(" Do widzenia");
+        String a ="Do widzenia";
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
